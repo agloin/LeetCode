12026d4b3e5df2ef8254b322937505eba7155275
agloin <darabeev@gmail.com>
2023-01-02 17:33:19 +0100

    delete

--- src/main/java/easyLeetCode/LengthOfLastWord.java
+++
@@ -1,9 +0,0 @@
-package easyLeetCode;
-
-public class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        String[] s1 = s.split(" ");
-        return s1[s1.length - 1].length();
-
-    }
-}
