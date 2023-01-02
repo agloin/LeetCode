04b8af61c4894be6888ceb8bf7444acad5417410
agloin <darabeev@gmail.com>
2023-01-02 17:12:25 +0100

    added links for LeetCode

--- src/test/java/easyLeetCode/CommonPrefixTest.java
+++ src/test/java/easyLeetCode/CommonPrefixTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//https://leetcode.com/problems/longest-common-prefix/
 class CommonPrefixTest {
 
     @Test
