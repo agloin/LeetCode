c21b6b9158f70419ed8056269fcc81491cf88cea
agloin <darabeev@gmail.com>
2023-01-05 10:52:02 +0100

    remove files

--- src/test/java/easyLeetCode/MergeTwoSortedLists/MergeTwoSortedListsTest.java
+++ src/test/java/easyLeetCode/MergeTwoSortedLists/MergeTwoSortedListsTest.java
@@ -1,12 +1,10 @@
 package easyLeetCode.MergeTwoSortedLists;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
 class ListNode {
     int val;
     easyLeetCode.MergeTwoSortedLists.ListNode next;
@@ -23,57 +21,135 @@ class ListNode {
         this.next = next;
     }
 }
+
+
 class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        LinkedList<Integer> arrayList = new LinkedList<>();
-        var answer = new ListNode();
 
-
-        if (list1 == null && list2 == null) {
-            return null;
-        } else if (list1 == null) {
+        if (list1 == null)
             return list2;
-        } else if (list2 == null) {
+        if (list2 == null)
             return list1;
+
+        ListNode head = new ListNode();
+
+        if (list1.val < list2.val) {
+            head.next = list1;
+            list1 = list1.next;
         } else {
-            while (list1 != null) {
-                while (list2 != null) {
-                    int val2 = list2.val;
-
-                    arrayList.add(val2);
-                    list2 = list2.next;
-                }
-                int val1 = list1.val;
-
-                arrayList.add(val1);
-                list1 = list1.next;
-
-            }
-            Collections.sort(arrayList);
-
-            LinkedList<ListNode> listOfAnswer = new LinkedList<>();
-
-            for (Integer integer : arrayList) {
-                answer = new ListNode(integer);
-                listOfAnswer.add(answer);
-            }
-            for (int i = 0; i < listOfAnswer.size() - 1; i++) {
-                listOfAnswer.get(i).next = listOfAnswer.get(i + 1);
-            }
-            return listOfAnswer.get(0);
+            head.next = list2;
+            list2 = list2.next;
+        }
+        if (list1 == null) {
+            head.next.next = list2;
+        }
+        if (list2 == null) {
+            head.next.next = list1;
         }
 
+        ListNode tmp = head.next;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tmp.next = list1;
+                list1 = list1.next;
+            } else {
+                tmp.next = list2;
+                list2 = list2.next;
+            }
+            tmp = tmp.next;
+
+            if (list1 == null) {
+                tmp.next = list2;
+            }
+            if (list2 == null) {
+                tmp.next = list1;
+            }
+        }
+        return head.next;
     }
+
 }
+
+
 class MergeTwoSortedListsTest {
 
     @Test
     void mergeTwoSortedListsTest() {
         MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
-
-        int[] nums1 = new int[]{1, 2, 3};
+        int[] nums1 = new int[]{1, 2, 4};
         int[] nums2 = new int[]{1, 3, 4};
-        int[] commonNums = new int[]{1, 1, 2, 3, 3, 4};
+
+        int[] commonNums = new int[]{1, 1, 2, 3, 4, 4};
+        ListNode list1 = intSortedArrayToListNode(nums1);
+        ListNode list2 = intSortedArrayToListNode(nums2);
+
+
+        var res = mergeTwoSortedLists.mergeTwoLists(list1, list2);
+
+
+        ListNode head;
+        head = intSortedArrayToListNode(commonNums);
+
+        while (head != null) {
+            assertEquals(head.val, res.val);
+            head = head.next;
+            res = res.next;
+        }
+    }
+
+    @Test
+    void test(){
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+        int[] nums3 = new int[]{2};
+        int[] nums4 = new int[]{1};
+        int[] commonNums = new int[]{1, 2};
+
+        ListNode list1 = intSortedArrayToListNode(nums3);
+        ListNode list2 = intSortedArrayToListNode(nums4);
+
+        var res = mergeTwoSortedLists.mergeTwoLists(list1, list2);
+
+        ListNode head;
+        head = intSortedArrayToListNode(commonNums);
+
+        while (head != null) {
+            assertEquals(head.val, res.val);
+            head = head.next;
+            res = res.next;
+        }
+    }
+
+    @Test
+    void mergeTwoSortedListsWithEmptyLists() {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        int[] nums1 = new int[]{};
+        int[] nums2 = new int[]{};
+        int[] commonNums = new int[]{};
+        ListNode list1 = intSortedArrayToListNode(nums1);
+        ListNode list2 = intSortedArrayToListNode(nums2);
+
+
+        var res = mergeTwoSortedLists.mergeTwoLists(list1, list2);
+
+
+        ListNode head;
+        head = intSortedArrayToListNode(commonNums);
+
+        while (head != null) {
+            assertEquals(head.val, res.val);
+            head = head.next;
+            res = res.next;
+        }
+    }
+
+    @Test
+    void mergeTwoSortedListsWithOneEmptyLists() {
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        int[] nums1 = new int[]{};
+        int[] nums2 = new int[]{0};
+        int[] commonNums = new int[]{0};
         ListNode list1 = intSortedArrayToListNode(nums1);
         ListNode list2 = intSortedArrayToListNode(nums2);
 
@@ -95,18 +171,18 @@ class MergeTwoSortedListsTest {
     ListNode intSortedArrayToListNode(int[] nums) {
         ListNode head = new ListNode();
         ListNode tmp = new ListNode();
+        int length = nums.length;
 
-        int i = 0;
-        for (int num : nums) {
-            head.val = num;
+//        int i = 0;
+        for (int i = 0; i < length; i++) {
+            head.val = nums[i];
             head.next = new ListNode();
-            if (i == 0) {
-                tmp = head;
-                i++;
-            }
-            head = head.next;
+            if (i == 0) tmp = head;
+
+
+            if (i != length - 1) head = head.next;
+            else head.next = null;
         }
-        tmp.next = null;
         return tmp;
     }
 
