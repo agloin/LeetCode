bc6433c7e37b4dbe5e85fe366561f2f730f81875
agloin <darabeev@gmail.com>
2023-01-09 18:52:40 +0100

    LengthOfLastWord with tests

--- src/test/java/easyLeetCode/LengthOfLastWordTest.java
+++ src/test/java/easyLeetCode/LengthOfLastWordTest.java
@@ -1,37 +1,47 @@
 package easyLeetCode;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //https://leetcode.com/problems/length-of-last-word/
 
 class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-        String[] s1 = s.split(" ");
-        int length = s1.length;
+        int i = s.length() - 1;
+        int res = 0;
 
-        if (length > 0) return s1[length - 1].length();
-        return 0;
+        if (i == 0 && s.charAt(i) != ' ' && s.charAt(i) != '\t') return 1;
+        else if (i == 0 && (s.charAt(i) == ' ' || s.charAt(i) == '\t')) return 0;
+
+        while (s.charAt(i) == ' ' || s.charAt(i) == '\t') {
+            i--;
+        }
+        while (i >= 0 && (s.charAt(i) != ' ' || s.charAt(i) == '\t')) {
+            res++;
+            i--;
+        }
+        return res;
     }
 }
+
 class LengthOfLastWordTest {
 
-    @Test
-    void lengthOfLastWord() {
-        String s1 = "Hello World";
-        String[] s11 = s1.split(" ");
-        int length11 = s11.length;
-
-        String s2 = "   fly me   to   the moon  ";
-        String[] s21 = s2.split(" ");
-        int length21 = s21.length;
-
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "Hello World",
+                    "   fly me   to   the moon  "
+            }
+    )
+    void lengthOfLastWord(String s) {
+        String[] s1 = s.split(" ");
         LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
 
-        assertEquals(s11[length11 - 1].length(), lengthOfLastWord.lengthOfLastWord(s1));
-        assertEquals(s21[length21 - 1].length(), lengthOfLastWord.lengthOfLastWord(s2));
+        assertEquals(s1[s1.length - 1].length(), lengthOfLastWord.lengthOfLastWord(s));
     }
 
     @Test
@@ -41,4 +51,19 @@ class LengthOfLastWordTest {
 
         assertEquals(0, lengthOfLastWord.lengthOfLastWord(s2));
     }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "a",
+                    "a "
+            }
+    )
+    void lengthOfLastWordWithOneSymbolAndSpaceInEnd(String s) {
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+
+        assertEquals(1, lengthOfLastWord.lengthOfLastWord(s));
+    }
+
+
 }
