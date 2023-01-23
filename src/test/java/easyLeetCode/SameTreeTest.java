966cbb1838453d1af1f139899d7cd3d9365d6fc2
agloin <darabeev@gmail.com>
2023-01-23 15:13:04 +0100

    SameTree wrong answer on Leet Code Platform

--- src/test/java/easyLeetCode/SameTreeTest.java
+++ src/test/java/easyLeetCode/SameTreeTest.java
@@ -1,8 +1,11 @@
 package easyLeetCode;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class TreeNode {
     int val;
@@ -46,38 +49,66 @@ class SameTree {
     }
 }
 
-// p =
-//  [390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,
-//  -187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,
-//  null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,
-//  null,1163,1445,1533,null,null,null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null
-//  ,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,
-//  3618,3751,null,4006,null,null,4246,null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,
-//  null,null,null,null,2393,2461,null,null,null,null,4250,null,null,null,null,2537]
-
-//  q =
-//  [390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,
-//  -187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,
-//  null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,
-//  null,1163,1445,1533,null,null,null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null
-//  ,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,
-//  3618,3751,null,4006,null,null,4246,null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,
-//  null,null,null,null,2461,2393,null,null,null,null,4250,null,null,null,null,2537]
-
-// p = [1,2], q = [1,null,2]
 public class SameTreeTest {
 
-    @Test
-    void test() {
-        TreeNode p = new TreeNode();
-        TreeNode q = new TreeNode();
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    """
+                            '[390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,
+                              -187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,
+                              null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,
+                              null,1163,1445,1533,null,null,null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null
+                              ,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,
+                              3618,3751,null,4006,null,null,4246,null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,
+                              null,null,null,null,2393,2461,null,null,null,null,4250,null,null,null,null,2537]',    '[390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,
+                              3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,-187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,
+                              null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,
+                              null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,null,1163,1445,1533,null,null,null,2111,2792,null,null,null,
+                              3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,
+                              null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,3618,3751,null,4006,null,null,4246,null,null,4554,null,
+                              null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,null,null,null,null,2461,2393,null,null,null,null,4250,
+                              null,null,null,null,2537]'"""
+            }
+    )
+    void test(String sp, String sq) {
+        TreeNode p;
+        TreeNode q;
+        SameTree sameTree = new SameTree();
 
-        ArrayList<Integer> nums = new ArrayList<>();
+        p = toTreeNode(sp);
+        q = toTreeNode(sq);
+
+        Assertions.assertFalse(sameTree.isSameTree(p, q));
 
     }
 
-    void fillTheTree(ArrayList<Integer> nums, TreeNode treeNode) {
+    TreeNode toTreeNode(String s) {
+        if (s.charAt(0) == '[') s = s.substring(1, s.length() - 1);
+        List<TreeNode> nodes = Arrays.stream(s.split(","))
+                .map(i -> i.trim().equals("null") ? null : new TreeNode(Integer.parseInt(i.trim())))
+                .toList();
+        int ln = 1;
+        int idx = 0;
+        while (idx < nodes.size()) {
+            int cnt = 0;
+            for (int i = 0; i < ln && idx + i < nodes.size(); i++) {
 
-
+                TreeNode node = nodes.get(idx + i);
+                if (node == null) continue;
+                int leftIndex = idx + ln + cnt * 2;
+                int rightIndex = leftIndex + 1;
+                cnt++;
+                if (leftIndex < nodes.size() && nodes.get(leftIndex) != null) {
+                    node.left = nodes.get(leftIndex);
+                }
+                if (rightIndex < nodes.size() && nodes.get(rightIndex) != null) {
+                    node.right = nodes.get(rightIndex);
+                }
+            }
+            idx += ln;
+            ln = cnt * 2;
+        }
+        return nodes.get(0);
     }
 }
