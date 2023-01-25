1fb41a817c9d8a8cc0899bc1c9f5682eeba5b855
agloin <darabeev@gmail.com>
2023-01-25 22:01:43 +0100

    SameTree wrong answer in the first test with large input

--- src/test/java/easyLeetCode/SameTreeTest.java
+++ src/test/java/easyLeetCode/SameTreeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 class TreeNode {
     int val;
@@ -28,50 +29,58 @@ class TreeNode {
 
 class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        var tmpQ = new TreeNode();
-        var tmpP = new TreeNode();
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
 
-        tmpQ.right = q;
-        tmpP.right = p;
-        while (p != null && q != null) {
-            if (p.val != q.val) return false;
-            p = p.left;
-            q = q.left;
-        }
-        if (p != null || q != null) return false;
+        Stack<TreeNode> stackP = new Stack<>();
+        Stack<TreeNode> stackQ = new Stack<>();
 
-        while (tmpQ.right != null && tmpP.right != null) {
-            if (tmpQ.right.val != tmpP.right.val) return false;
-            tmpQ.right = tmpQ.right.right;
-            tmpP.right = tmpP.right.right;
+        stackP.push(p);
+        stackQ.push(q);
+
+        while (!stackP.empty() && !stackQ.empty()) {
+            TreeNode treeNodeP = stackP.pop();
+            TreeNode treeNodeQ = stackQ.pop();
+
+            if (treeNodeP.val != treeNodeQ.val) return false;
+            if (treeNodeP.left != null || treeNodeQ.left != null) {
+                stackP.push(treeNodeP.left);
+                stackQ.push(treeNodeQ.left);
+            } else return false;
+
+
+            if (treeNodeP.right != null && treeNodeQ.right != null) {
+                stackP.push(treeNodeP.right);
+                stackQ.push(treeNodeQ.right);
+            } else return false;
         }
-        return tmpQ.right == null && tmpP.right == null;
+        return stackP.empty() && stackQ.empty();
     }
 }
 
 public class SameTreeTest {
-
     @ParameterizedTest
     @CsvSource(
             value = {
                     """
-                            '[390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,
-                              -187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,
-                              null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,
-                              null,1163,1445,1533,null,null,null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null
-                              ,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,
-                              3618,3751,null,4006,null,null,4246,null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,
-                              null,null,null,null,2393,2461,null,null,null,null,4250,null,null,null,null,2537]',    '[390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,
-                              3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,-187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,
-                              null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,
-                              null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,null,1163,1445,1533,null,null,null,2111,2792,null,null,null,
-                              3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,
-                              null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,3618,3751,null,4006,null,null,4246,null,null,4554,null,
-                              null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,null,null,null,null,2461,2393,null,null,null,null,4250,
-                              null,null,null,null,2537]'"""
+                            '[390,255,2266,-273,337,1105,3440,-425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,
+                            null,null,-187,152,395,null,728,977,1270,null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,
+                            null,1460,1663,null,1838,2891,null,null,null,null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,
+                            843,958,null,null,null,1163,1445,1533,null,null,null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,
+                            null,null,1216,null,null,1522,null,1889,2238,2558,2832,null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,
+                            2364,2678,null,null,null,3618,3751,null,4006,null,null,4246,null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,
+                            null,null,null,null,4253,null,null,null,null,2393,2461,null,null,null,null,4250,null,null,null,null,2537]',    '[390,255,2266,-273,337,1105,3440,
+                            -425,4113,null,null,600,1355,3241,4731,-488,-367,16,null,565,780,1311,1755,3075,3392,4725,4817,null,null,null,null,-187,152,395,null,728,977,1270,
+                            null,1611,1786,2991,3175,3286,null,164,null,null,4864,-252,-95,82,null,391,469,638,769,862,1045,1138,null,1460,1663,null,1838,2891,null,null,null,
+                            null,3296,3670,4381,null,4905,null,null,null,-58,null,null,null,null,null,null,null,null,734,null,843,958,null,null,null,1163,1445,1533,null,null,
+                            null,2111,2792,null,null,null,3493,3933,4302,4488,null,null,null,null,null,null,819,null,null,null,null,1216,null,null,1522,null,1889,2238,2558,2832,
+                            null,3519,3848,4090,4165,null,4404,4630,null,null,null,null,null,null,1885,2018,2199,null,2364,2678,null,null,null,3618,3751,null,4006,null,null,4246,
+                            null,null,4554,null,null,null,1936,null,null,null,null,2444,2642,2732,null,null,null,null,null,null,null,4253,null,null,null,null,2461,2393,null,null,
+                            null,null,4250,null,null,null,null,2537]', false
+                              """
             }
     )
-    void test(String sp, String sq) {
+    void sameTreeLargeInputTest(String sp, String sq, boolean res) {
         TreeNode p;
         TreeNode q;
         SameTree sameTree = new SameTree();
@@ -79,8 +88,27 @@ public class SameTreeTest {
         p = toTreeNode(sp);
         q = toTreeNode(sq);
 
-        Assertions.assertFalse(sameTree.isSameTree(p, q));
+        Assertions.assertEquals(res, sameTree.isSameTree(p, q));
+    }
 
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "null, null, true",
+                    "null, '[390]', false",
+                    "'[1,2,3]', '[1,2,3]', true",
+                    "'[1,2]', '[1,null,3]', false",
+            }
+    )
+    void sameTreeTest(String sp, String sq, boolean res) {
+        TreeNode p;
+        TreeNode q;
+        SameTree sameTree = new SameTree();
+
+        p = toTreeNode(sp);
+        q = toTreeNode(sq);
+
+        Assertions.assertEquals(res, sameTree.isSameTree(p, q));
     }
 
     TreeNode toTreeNode(String s) {
